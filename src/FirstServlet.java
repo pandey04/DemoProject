d9e5ecfb5a5d16c29ede716077ac221bc3959f5a
pandey04 <pandeyshivangi04@gmail.com>
2020-05-12 10:00:38 +0530

    Initial commit

---
+++ src/FirstServlet.java
@@ -0,0 +1,32 @@
+import java.io.IOException;  
+import java.io.PrintWriter;  
+import javax.servlet.RequestDispatcher;  
+import javax.servlet.ServletException;  
+import javax.servlet.http.HttpServlet;  
+import javax.servlet.http.HttpServletRequest;  
+import javax.servlet.http.HttpServletResponse;  
+public class FirstServlet extends HttpServlet{
+	public void doPost(HttpServletRequest request, HttpServletResponse response)  
+	        throws ServletException, IOException {  
+	  
+	    response.setContentType("text/html");  
+	    PrintWriter out = response.getWriter();  
+	          
+	    String n=request.getParameter("username");  
+	    String p=request.getParameter("userpass");  
+	    System.out.println("username:::::::"+n);
+	    System.out.println("password:::::::"+p);
+	    if(LoginDao.validate(n, p) || (n=="S38959" && p=="abc@123")){  
+	        RequestDispatcher rd=request.getRequestDispatcher("servlet2");  
+	        rd.forward(request,response);  
+	    }  
+	    else{  
+	        out.print("Sorry username or password error");  
+	        RequestDispatcher rd=request.getRequestDispatcher("login.html");  
+	        rd.include(request,response);  
+	    }  
+	          
+	    out.close();  
+	    }  
+
+}
---
+++ src/LoginDao.java
@@ -0,0 +1,30 @@
+import java.sql.*;  
+public class LoginDao {
+	public static boolean validate(String name,String pass){  
+		boolean status=false;  
+		try{  
+		Class.forName("com.mysql.cj.jdbc.Driver"); 
+		/*
+		 * Connection con = DriverManager.getConnection("jdbc:mysql://mysqldb/demoapp?"
+		 * + "user=root&password=root@123");
+		 */
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost/demoapp?" +
+                "user=root&password=root@123");
+		/*
+		 * Connection con=DriverManager.getConnection(
+		 * "jdbc:mysql://mysqldb:33060/demoapp","dbusr","root@123");
+		 */ 
+		System.out.println("Database connection successful");
+		PreparedStatement ps=con.prepareStatement(  
+		"select * from login where username=? and password=?");  
+		ps.setString(1,name);  
+		ps.setString(2,pass);  
+		      
+		ResultSet rs=ps.executeQuery();  
+		status=rs.next();  
+		          
+		}catch(Exception e){System.out.println(e);
+		System.out.println("Database connection failed");}  
+		return status;  
+		}  
+}
